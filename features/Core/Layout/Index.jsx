a256df9c0ffdedadd9c6f100c95119dfd87078ae
Arun AS <ognitiotechnologies@gmail.com>
2021-07-13 18:16:43 +0530

    siderbar, appbar splitted from layout

--- features/Core/Layout/Index.jsx
+++ features/Core/Layout/Index.jsx
@@ -1,19 +1,9 @@
 import React, { useState } from 'react';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+
 import AppBar from './AppBar';
+import Sidebar from './Sidebar';
 
 const drawerWidth = 240;
 
@@ -69,7 +59,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 const Layout = (props) => {
   const classes = useStyles();
-  const theme = useTheme();
+
   const [open, setOpen] = useState(true);
 
   const handleDrawerOpen = () => {
@@ -82,49 +72,8 @@ const Layout = (props) => {
 
   return (
     <div className={classes.root}>
-      <CssBaseline />
       <AppBar handleDrawerOpen={handleDrawerOpen} open={open} />
-      <Drawer
-        className={classes.drawer}
-        variant='persistent'
-        anchor='left'
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? (
-              <ChevronLeftIcon />
-            ) : (
-              <ChevronRightIcon />
-            )}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+      <Sidebar handleDrawerClose={handleDrawerClose} open={open} />
       <main
         className={clsx(classes.content, {
           [classes.contentShift]: open,
